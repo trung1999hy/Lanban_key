839c970bae138f59643520a0749a3493321ba486
Tran nam <anhnam2.2k.yn@gmail.com>
2023-07-31 20:58:24 +0700

    init

--- app/src/main/java/com/example/keyboardtheme/inapp/Constants.kt
+++ app/src/main/java/com/example/keyboardtheme/inapp/Constants.kt
@@ -3,10 +3,10 @@ package com.example.keyboardtheme.inapp
 object Constants {
     const val KEY_5_COIN = "Key_in_app_5coin" // 5 coin
     const val KEY_10_COIN = "Key_in_app_10_coin1" // 100 coin
-    const val KEY_20_COIN = "Key_in_app_20_coin" // 150 coin
-    const val KEY_50_COIN = "Key_in_app_50_coin" // 300 coin
-    const val KEY_100_COIN = "Key_in_app_100_coin" // 500 coin
-    const val KEY_150_COIN = "Key_in_app_150_coin" // 700 coin
-    const val KEY_200_COIN = "Key_in_app_200_coin" // 999 coin
-    const val KEY_500_COIN = "Key_in_app_500_coin"
+    const val KEY_20_COIN = "Key_in_app_20_coin1" // 150 coin
+    const val KEY_50_COIN = "Key_in_app_50_coin1" // 300 coin
+    const val KEY_100_COIN = "Key_in_app_100_coin1" // 500 coin
+    const val KEY_150_COIN = "Key_in_app_150_coin1" // 700 coin
+    const val KEY_200_COIN = "Key_in_app_200_coin1" // 999 coin
+    const val KEY_500_COIN = "Key_in_app_500_coin1"
 }
