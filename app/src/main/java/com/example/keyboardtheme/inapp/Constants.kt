8d9adc63d5c52f948e15bdbc336f8175bbb0926b
Trần Nam <65379025+tranam1312@users.noreply.github.com>
2023-08-14 14:28:25 +0700

    Update Constants.kt

--- app/src/main/java/com/example/keyboardtheme/inapp/Constants.kt
+++ app/src/main/java/com/example/keyboardtheme/inapp/Constants.kt
@@ -1,12 +1,12 @@
 package com.example.keyboardtheme.inapp
 
 object Constants {
-    const val KEY_5_COIN = "Key_in_app_5coin" // 5 coin
-    const val KEY_10_COIN = "Key_in_app_10_coin1" // 100 coin
-    const val KEY_20_COIN = "Key_in_app_20_coin1" // 150 coin
-    const val KEY_50_COIN = "Key_in_app_50_coin1" // 300 coin
-    const val KEY_100_COIN = "Key_in_app_100_coin1" // 500 coin
-    const val KEY_150_COIN = "Key_in_app_150_coin1" // 700 coin
-    const val KEY_200_COIN = "Key_in_app_200_coin1" // 999 coin
-    const val KEY_500_COIN = "Key_in_app_500_coin1"
+    const val KEY_5_COIN = "key_in_app_5coin" // 5 coin
+    const val KEY_10_COIN = "key_in_app_10_coin1" // 100 coin
+    const val KEY_20_COIN = "key_in_app_20_coin1" // 150 coin
+    const val KEY_50_COIN = "key_in_app_50_coin1" // 300 coin
+    const val KEY_100_COIN = "key_in_app_100_coin1" // 500 coin
+    const val KEY_150_COIN = "key_in_app_150_coin1" // 700 coin
+    const val KEY_200_COIN = "key_in_app_200_coin1" // 999 coin
+    const val KEY_500_COIN = "key_in_app_500_coin1"
 }
